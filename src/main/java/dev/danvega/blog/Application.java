4a25fd7690aed65e9cd81fc98c9b8a3926ee7f79
Sebastian Braun <79113346+sebastian-braun@users.noreply.github.com>
2023-04-26 11:42:24 +0200

    test

--- src/main/java/dev/danvega/blog/Application.java
+++ src/main/java/dev/danvega/blog/Application.java
@@ -19,13 +19,13 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Profile({"dev","prod"})
+	@Profile({"prod"})
 	@Bean
 	CommandLineRunner run(PostRepository postRepository, AuthorRepository authorRepository) {
 		return args -> {
 			AggregateReference<Author,Integer> author = AggregateReference.to(authorRepository.save(new Author(null, "Dan", "Vega", "danvega@gmail.com", "dvega")).id());
 
-			Post post = new Post( "Dan's First Post", "This is Dan's First Post",author);
+			Post post = new Post( "Sebs's First Post", "This is Dan's First Post",author);
 			post.addComment(new Comment( "Dan", "This is a comment"));
 			post.addComment(new Comment( "John", "This is another comment"));
 			postRepository.save(post);
